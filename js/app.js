fe152cefb28a74cfdb4edb654eb9f86ddc197f20
A-Alsaffaf <ahmedy.saffaf@gmail.com>
2026-08-03 09:15:43 +0300

    feat: add handleClick, updateMessage, updateBoard, placePiece functions along with event listener to the board element

--- js/app.js
+++ js/app.js
@@ -1,20 +1,90 @@
 /*-------------------------------- Constants --------------------------------*/
-
+const initBoard = Array(9).fill(null)
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
 
 /*---------------------------- Variables (state) ----------------------------*/
+let board = initBoard
+console.log(board);
+let turn = 'X'
+let winner = false
+let tie = false 
+
 
 
 
 /*------------------------ Cached Element References ------------------------*/
-
+const boardEl = document.querySelector('.board')
+const squareEls = document.querySelectorAll('.sqr')
+const messageEl = document.querySelector('#message')
 
 
 /*-------------------------------- Functions --------------------------------*/
+function init() {
+
+
+render()    
+}
+
+function render() {
+    updateBoard()
+    updateMessage()
+}
+
+function updateBoard() {
+    board.forEach((sqr, idx) => {
+        squareEls[idx].textContent = sqr
+    })
+}
+
+function updateMessage() {
+    if (!winner && !tie) {
+        messageEl.textContent = `It's ${turn}'s turn!`
+    } else if (winner) {
+        messageEl.textContent = `${turn} wins!`
+    } else if (tie && !winner) {
+        messageEl.textContent = `It's a tie!`
+    }
+}
+
+function handleClick(event) {
+    const clickedElement = event.target
+    console.log(clickedElement);
+    if (winner || tie) {
+        console.log('game is over');
+        return
+    }else if (!clickedElement.classList.contains('sqr')) {
+        console.log('its not a square');
+        return
+    }
+    else if (clickedElement.classList.contains('sqr')) {
+        const squareIndex = clickedElement.id
+        if (board[squareIndex] === 'X' || board[squareIndex] === 'O') {
+            console.log('Square is already taken');
+            return
+        }else if (board[squareIndex] === null) {
+            placePiece(squareIndex)
+        }
+    }
+}
+
+function placePiece(idx) {
+    board[idx] = turn
+    console.log(board);
+}
 
 
 
 /*----------------------------- Event Listeners -----------------------------*/
 
-
+boardEl.addEventListener('click', handleClick)
 
